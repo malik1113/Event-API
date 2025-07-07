d3be6c171eb728815491f36b1620d32fefc2cc20
malik1113 <malikroberts007@gmail.com>
2025-07-07 12:03:22 -0400

    Initialize our Event-API project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const logger = require("morgan");
+const connectToMongoDB = require("./database/connectToMongoDB")
+
+const app = express()
+const PORT = 3000;
+
+app.use(logger("dev"))
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`)
+    connectToMongoDB();
+})
